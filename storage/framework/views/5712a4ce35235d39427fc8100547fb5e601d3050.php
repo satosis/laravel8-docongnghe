36652157c61844f4fd5dfba86c1a855134bbe8e0
hungnm2000 <hungnm2000@vnpt.vn>
2025-04-23 16:23:04 +0700

    update

--- storage/framework/views/5712a4ce35235d39427fc8100547fb5e601d3050.php
+++
@@ -1,8 +0,0 @@
-<hr>
-<a class="btn btn-google js-maintain" href="">
-	<i class="fa fa-google-plus"></i> Google
-</a><br>
-<a class="btn btn-facebook js-maintain" href="">
-	<i class="fa fa-facebook"></i> Facebook
-</a><br>
-<?php /**PATH D:\project\laravel8-banquanao\resources\views/auth/include/_inc_social.blade.php ENDPATH**/ ?>
