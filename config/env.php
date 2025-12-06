4bd2217f118fdcf45866815d77895a32f8e1b73e
satosis <hung0913003358@gmail.com>
2025-12-06 19:31:47 +0700

    update

--- config/env.php
+++ config/env.php
@@ -1,10 +1,5 @@
 <?php
 return [
     'app_url'   => env('APP_URL',null),
-    'vnpay' => [
-        'code'        => env('VNPAY_CODE',null),
-        'secret'     => env('VNPAY_SECRET', null),
-        'url'     => "http://sandbox.vnpayment.vn/paymentv2/vpcpay.html",
-        'callback' => env('VNPAY_CALLBACK_URL', null),
-    ],
+   
 ];
